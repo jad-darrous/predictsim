e15e7991a0ea06d47844b129ac453155bad14ae1
Valentin Reis <valentin.reis@gmail.com>
2014-06-02 16:47:55 +0200

    need to add all the funny features

--- learners/randomforestpredict.py
+++ learners/randomforestpredict.py
@@ -23,8 +23,6 @@ arguments = docopt(__doc__, version='1.0.0rc2')
 if '--verbose' in arguments.keys():
     print(arguments)
 
-
-
 import numpy as np
 from swfpy import io
 data=io.swfopen(arguments["<filename>"],output="np.array")
@@ -35,8 +33,16 @@ data=data.view(np.int32).reshape(data.shape + (-1,))
 #TODO: hour of day, day of week.
 #subtime=np.
 
-np.delete(data,0,1)
 
+#0-'job_id',1'submit_time',2-'wait_time',3-'run_time',4-'proc_alloc',5-'cpu_time_used',6-'mem_used',7'proc_req',8'time_req',9-'mem_req',10-'status',11'user_id',12'group_id',13-'exec_id',14-'queue_id',15-'partition_id',16-'previous_job_id',17-'think_time'.
+X=np.delete(data,[0,2,3,4,5,6,9,10,13,14,15,16,17],1)
+s=3600*24
+X[:,0]=X[:,0] %s
+i=int(len(X)*.8)
+Xlearn=X[1:i:1,:]
+Xtest=X[i:len(X),:]
+
+y=data[:,4]
 
 #interactive?
 if '--interactive' in arguments.keys():
