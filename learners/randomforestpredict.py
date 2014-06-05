2e2568c352b7b453d0d8be116ac17a03a09399ea
Valentin Reis <valentin.reis@gmail.com>
2014-06-05 16:34:00 +0200

    gid

--- learners/randomforestpredict.py
+++ learners/randomforestpredict.py
@@ -105,7 +105,7 @@ if encoding=="onehot":
     enc_user_id = preprocessing.OneHotEncoder()
     X_onehot_user_id =np.array( enc_user_id.fit_transform(np.reshape(X[:,3],(-1,1))).toarray())
     enc_group_id = preprocessing.OneHotEncoder()
-    X_onehot_group_id =np.array( enc_onehot_group_id.fit_transform(np.reshape(X[:,4],(-1,1))).toarray())
+    X_onehot_group_id =np.array( enc_group_id.fit_transform(np.reshape(X[:,4],(-1,1))).toarray())
     enc_day_of_week = preprocessing.OneHotEncoder()
     X_onehot_day_of_week =np.array( enc_onehot_day_of_week.fit_transform(np.reshape(X[:,6],(-1,1))).toarray())
     enc_last_status = preprocessing.OneHotEncoder()
