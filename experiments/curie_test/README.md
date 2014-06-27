f1c66b46be9e85fd8d250bfc7ee2fb9af6c53ee5
Valentin Reis <valentin.reis@gmail.com>
2014-06-27 23:34:46 +0200

    commit sample swf folder

---
+++ experiments/curie_test/README.md
@@ -0,0 +1,3 @@
+use like this:
+./make_swf_memory.sh swf/curie.swf prediction_results/curie_prediction_sgd > predicted_swf/curie.swf
+where curie.swf is a full swf file, and curie_prediction_sgd is the column for replacing req_time.
--- experiments/curie_test/make_swf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-awk '{print $1,$2,$3,$4,$5,$6,$7,$8}' curie_cut.swf > /tmp/swftmp1
-awk '{print $10,$11,$12,$13,$14,$15,$16,$17,$18}' curie_cut.swf > /tmp/swftmp1b
-paste /tmp/swftmp1 prediction_sgd /tmp/swftmp1b
-rm /tmp/swftmp1 /tmp/swftmp1b
