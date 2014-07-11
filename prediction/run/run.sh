03cae7b0cf33aebabd66ab29ef1471b941a3b4e9
Valentin Reis <valentin.reis@gmail.com>
2014-07-11 13:57:49 +0200

    prediction results

--- prediction/run/run.sh
+++ prediction/run/run.sh
@@ -36,18 +36,18 @@ mkdir -p $simulated_files_dir
 echoinred "==== RUNNING SIMULATIONS ===="
 
 
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_epsilon_insensitive l2 onehot 86400 93312   >out_2\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_epsilon_insensitive l1 onehot 86400 93312   >out_3\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_epsilon_insensitive elasticnet onehot 86400 93312   >out_4\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_loss l2 onehot 86400 93312   >out_5\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_loss l1 onehot 86400 93312   >out_6\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd squared_loss elasticnet onehot 86400 93312   >out_7\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd huber l2 onehot 86400 93312   >out_8\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd huber l2 onehot 86400 93312   >out_9\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd huber l1 onehot 86400 93312   >out_10\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd epsilon_insensitive elasticnet onehot 86400 93312   >out_11\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd epsilon_insensitive l1 onehot 86400 93312   >out_12\
-job_pool_run ./predictor.py ../../experiments/CEA-curie_cut/original_swf/curie_cut.swf ../experiments/CEA-curie_cut/original_swf/extracted_features sgd epsilon_insensitive elasticnet onehot 86400 93312   >out_13
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_epsilon_insensitive l2 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_epsilon_insensitive l1 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_epsilon_insensitive elasticnet onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_loss l2 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_loss l1 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd squared_loss elasticnet onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd huber l2 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd huber l2 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd huber l1 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd epsilon_insensitive elasticnet onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd epsilon_insensitive l1 onehot 86400 93312   >out
+job_pool_run ../predictor.py ../../data/CEA-curie_sample/original_swf/curie_sample.swf ../../data/CEA-curie_sample/original_swf/extracted_features_curie_sample sgd epsilon_insensitive elasticnet onehot 86400 93312   >out
 
 
 # wait until all jobs complete before continuing
@@ -58,12 +58,7 @@ job_pool_shutdown
 # check the $job_pool_nerrors for the number of jobs that exited non-zero
 echo "job_pool_nerrors: ${job_pool_nerrors}"
 
+echo "__OK__"
 
-echoinred "==== RUNNING ANALYSIS ===="
-echoinblue Rscript simul_analysis.R $final_csv_file $simulated_files_dir/*.swf
-Rscript simul_analysis.R $final_csv_file $simulated_files_dir/*.swf
-
-
-echoinred "==== THE END. Results are in results.csv ===="
 
 
