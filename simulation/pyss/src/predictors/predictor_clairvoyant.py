71976d14ec10e8c960c5af7d6ab810c65b2a2fc7
David Glesser <david.glesser@imag.fr>
2014-07-17 15:33:16 +0200

    Nice spaces.

--- simulation/pyss/src/predictors/predictor_clairvoyant.py
+++ simulation/pyss/src/predictors/predictor_clairvoyant.py
@@ -1,8 +1,4 @@
-
-
 from predictor import Predictor
-
-
 
 class PredictorClairvoyant(Predictor):
 	"""
@@ -11,14 +7,13 @@ class PredictorClairvoyant(Predictor):
 
 	def __init__(self, num_processors, max_runtime=None):
 		pass
-	
+
 	def predict(self, job, current_time):
 		"""
 		Modify the estimate_runtime of job.
 		Called when a job is submitted to the system.
 		"""
 		job.predicted_run_time = job.actual_run_time
-	
+
 	def fit(self, job, current_time):
 		pass
-
