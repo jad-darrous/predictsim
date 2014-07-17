e550c29237e13767fa013783373389b2849915ed
Valentin Reis <valentin.reis@gmail.com>
2014-07-17 15:30:22 +0200

    whitespace

--- simulation/pyss/src/predictors/predictor.py
+++ simulation/pyss/src/predictors/predictor.py
@@ -1,7 +1,3 @@
-
-
-
-
 class Predictor(object):
 	"""
 	This is an "interface" to declare a predictor
@@ -9,19 +5,17 @@ class Predictor(object):
 
 	def __init__(self, num_processors, max_runtime=None):
 		print("Do it")
-	
+
 	def predict(self, job, current_time):
 		"""
 		Return the estimate_runtime of job.
 		Called when a job is submitted to the system.
 		"""
 		print("Do it")
-	
+
 	def fit(self, job, current_time):
 		"""
 		Add a job to the learning algorithm.
 		Called when a job end.
 		"""
 		print("Do it")
-
-
