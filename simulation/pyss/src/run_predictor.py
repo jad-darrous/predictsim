c3e53c6db1f6db732293472519039f5edba2cc20
Valentin Reis <valentin.reis@gmail.com>
2014-07-18 13:16:56 +0200

    run_predictor added

--- simulation/pyss/src/run_predictor.py
+++ simulation/pyss/src/run_predictor.py
@@ -5,8 +5,7 @@ Runtime predictor tester.
 
 Usage:
     predictor_tester.py <filename> <output_folder> tsafrir [-i] [-v]
-    predictor_tester.py <filename> <output_folder> sgd <loss> <penalty> <max_cores> [-i] [-v]
-    predictor_tester.py <filename>  <output_folder> passive-aggressive <loss> <max_cores> [-i] [-v]
+    predictor_tester.py <filename> <output_folder> clairvoyant[-i] [-v]
 
 Options:
     -h --help                                      Show this help message and exit.
@@ -17,140 +16,112 @@ Options:
     loss                                           for sgd: in  'squared_loss', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive', 'maeloss'.
                                                    for passive-aggressive: in 'epsilon_insensitive', 'squared_epsilon_insensitive'
     penalty                                        in 'l2' , 'l1' , 'elasticnet'
+    max_cores                                      can be a number, or "auto".
+
+TODO:
+    predictor_tester.py <filename> <output_folder> sgd <loss> <penalty> <max_cores> [-i] [-v]
+    predictor_tester.py <filename> <output_folder> passive-aggressive <loss> <max_cores> [-i] [-v]
 
 '''
-from docopt import docopt
-arguments = docopt(__doc__, version='1.0.0rc2')
-
+from base.docopt import docopt
 from base.prototype import _job_inputs_to_jobs
 from base.workload_parser import parse_lines
+from base.np_printutils import array_to_file
+from base.simpy import Environment,simulate,Monitor
+from base.simpy.util import start_delayed
 
+#Retrieve arguments
+arguments = docopt(__doc__, version='1.0.0rc2')
+
+#Manage Verbosity
 if arguments['--verbose']==True:
+    print("You asked for verbose output. File \"predictor.log\" will be created in this directory.")
     print(arguments)
     import warnings
+    import logging
+    global_logger = logging.getLogger('global')
+    hdlr = logging.FileHandler('predictor.log')
+    formatter = logging.Formatter('%(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    global_logger.addHandler(hdlr)
+    def global_log(msg):
+        prefix='%.1f'%env.now
+        global_logger.info(prefix+': '+msg)
+else:
+    def global_log(msg):
+        pass
 
+#argement management: max_cores
 if arguments['<max_cores>']==auto:
     with input_file as open(arguments['<filename>']):
+        num_processors=None
         for line in input_file:
-                if(line.lstrip().startswith(';')):
-                        if(line.lstrip().startswith('; MaxProcs:')):
-                                numproc = int(line.strip()[11:])
-                                break
-                        else:
-                                continue
+            if(line.lstrip().startswith(';')):
+                if(line.lstrip().startswith('; MaxProcs:')):
+                    num_processors = int(line.strip()[11:])
+                    break
                 else:
-                        break
-        if options.num_processors is None:
-                parser.error("missing num processors")
-elif eval(arguments['<max_cores>']):
-    numproc=
+                    continue
+            else:
+                break
+        if num_processors is None:
+            raise ValueError("missing num processors")
+elif arguments['<max_cores>'].isdigit():
+    num_processors=eval(arguments['<max_cores>'])
+else:
+    raise ValueError("<max_cores> must be an integer or \"auto\"")
 
-with f as  open(arguments['<filename>'], 'rt'):
-    jobs = _job_inputs_to_jobs(parse_lines(input_file), options.num_processors),
-
-
-if arguments["tsafrir"]:
-
-
-elif tool in ["sgd","passive-aggressive"]:
-    #___ONLINE LEARNING___
-
-    from simpy import Environment,simulate,Monitor
-    from swfpy import io
-    if arguments['--verbose']==True:
-        import logging
-    from simpy.util import start_delayed
-
-    if arguments['--verbose']==True:
-        global_logger = logging.getLogger('global')
-        hdlr = logging.FileHandler('predictor.log')
-        formatter = logging.Formatter('%(levelname)s %(message)s')
-        hdlr.setFormatter(formatter)
-        global_logger.addHandler(hdlr)
-    #Getting a simulation environment
-    env = Environment()
-    #logging function
-    if arguments['--verbose']==True:
-        def global_log(msg):
-            prefix='%.1f'%env.now
-            global_logger.info(prefix+': '+msg)
-    else:
-        def global_log(msg):
-            pass
-
-    if tool=="sgd":
-        #___SGD___
-        print("sgd")
-
-        loss=arguments["<loss>"]
-        if loss not in [ 'squared_loss', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive','maeloss']:
-            raise ValueError("invalid loss function")
-
-        penalty=arguments["<penalty>"]
-        if penalty not in ['l2' , 'l1' , 'elasticnet']:
-            raise ValueError("invalid penalty function")
-
-        model=SGDRegressor(loss=loss, penalty=penalty, alpha=0.0001, l1_ratio=0.15, fit_intercept=True, n_iter=5, shuffle=False, verbose=0, epsilon=0.1, random_state=None, learning_rate='invscaling', eta0=0.01, power_t=0.25, warm_start=False)
-    if tool=="passive-aggressive":
-        #___PASSIVE_AGGRESSIVE___
-
-        loss=arguments["<loss>"]
-        if loss not in [ 'epsilon_insensitive', 'squared_epsilon_insensitive']:
-            raise ValueError("invalid loss function")
-        model=PassiveAggressiveRegressor(C=1.0, fit_intercept=True, n_iter=5, shuffle=False, verbose=0, loss=loss, epsilon=0.1, random_state=None, class_weight=None, warm_start=False)
-
-    pred=[]
-    flag_bootstrapped=False
-    def job_process(i):
-        global flag_bootstrapped
-        j=Xf[i]
-        wait_time=data['wait_time'][i]
-        run_time=data['run_time'][i]
-        submit_time=data['submit_time'][i]
-
-        yield env.timeout(submit_time)
-        if flag_bootstrapped:
-            #print("predicting")
-            pred.append(min(abs(model.predict(j)),max_runtime,data['time_req'][i]))
-        else:
-            pred.append(0)
-
-        yield env.timeout(wait_time+run_time)
-        #print('4: time is %s,i= %s' % (env.now, i))
-        #print(j)
-        #print(Xf[i][4])
-        #print(Xf_tsafir_mean3[i])
-        #for k in range(0,len(np.array([j]))):
-                #if np.array([j])[k] <-1 or np.array([j])[k]>1:
-                    #print("k %s vector %s"%(k,np.array([j])))
-
-        model.partial_fit(np.array([j]),np.array([yf[i]]))
-        #print('5: time is %s,i= %s' % (env.now, i))
-
-        if not flag_bootstrapped:
-            flag_bootstrapped=True
-        if i % 1000==0:
-            print "processed %s jobs so far" %i
-
-    i=0
-    for i in range(len(X)):
-        env.start(job_process(i))
-        i=i+1
-
-    simulate(env)
-
+def iprint(p=None):
+    """interactive print: switch to interactive python shell if --interactive is asked."""
+    if not p==None:
+        print(p)
     if arguments['--interactive']==True:
-        print(arguments)
         from IPython import embed
         embed()
 
-    if tool=="sgd":
-        array_to_file(pred,arguments["<output_folder>"]+"/prediction_%s_%s_%s" %(tool,loss,penalty))
-    elif tool=="passive-aggressive":
-        array_to_file(pred,arguments["<output_folder>"]+"/prediction_%s_%s" %(tool,loss))
+iprint("Opening the swf file.")
+with f as  open(arguments['<filename>'], 'rt'):
+    jobs = _job_inputs_to_jobs(parse_lines(input_file), options.num_processors),
+print("Parsed swf file.")
 
-#interactive?
+print("Choosing predictor.")
+predictor=None
+if arguments["tsafrir"]:
+    from predictors.predictor_tsafrir import PredictorTsafrir
+    predictor=PredictorTsafrir(num_processors)
+if arguments["clairvoyant"]:
+    from predictors.predictor_tsafrir import PredictorClairvoyant
+    predictor=PredictorClairvoyant(num_processors)
+if predictor==None:
+    raise ValueError("no valid predictor specified")
+iprint("Predictor created.")
+
+#Getting a simulation environment
+env = Environment()
+
+pred=[]
+def job_process(j):
+    yield env.timeout(submit_time)
+    pred.append(predictor.predict(j))
+    yield env.timeout(wait_time+run_time)
+    predictor.fit(np.array([j]),np.array([yf[i]]))
+
+#Starting the replay
+for job in jobs:
+    env.start(job_process(job))
+simulate(env)
+
 if arguments['--interactive']==True:
     print(arguments)
     from IPython import embed
     embed()
+
+if tool=="tsafrir":
+    array_to_file(pred,arguments["<output_folder>"]+"/prediction_tsafrir")
+if tool=="clairvoyant":
+    array_to_file(pred,arguments["<output_folder>"]+"/prediction_clairvoyant")
+#if tool=="sgd":
+    #array_to_file(pred,arguments["<output_folder>"]+"/prediction_%s_%s_%s" %(tool,loss,penalty))
+#elif tool=="passive-aggressive":
+    #array_to_file(pred,arguments["<output_folder>"]+"/prediction_%s_%s" %(tool,loss))
+
