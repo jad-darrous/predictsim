ba3427fdffb772b44c3a6d558abbc4f64de160bc
Valentin Reis <valentin.reis@gmail.com>
2014-09-01 18:26:26 +0200

    remove useless predictor, and start to store data in a prototype predictor.

--- simulation/pyss/src/predictors/predictor_sgdlinear.py
+++ simulation/pyss/src/predictors/predictor_sgdlinear.py
@@ -4,56 +4,41 @@ import numpy as np
 class PredictorSGDLinear(Predictor):
     n_features=2
 
-    def __init__(self,max_procs=None, max_runtime=None, loss="squared_loss", eta=0.01, regularization="l1",alpha=1,beta=0,verbose=True):
-        self.user_run_time_prev = {}
-        self.user_run_time_last = {}
-        if loss=="squared_loss":
-            self.dloss=self.d_squared_loss
-        else:
-            raise ValueError("only squared_loss is supported")
-        assert isinstance(eta,int) or isinstance(eta,float)
-        self.eta=eta
-        self.w=np.zeros(self.n_features,dtype=np.float32)
+    def __init__(self,max_procs=None, max_runtime=None,verbose=True):
+        self.user_run_time_last3 = {}
+        self.user_run_time_last2 = {}
+        self.user_run_time_last1 = {}
 
-    def d_squared_loss(self, p, y):
-        return p - y
 
-    def make_x(self,job):
-        x=np.empty(self.n_features,dtype=np.float32)
+    #def make_x(self,job):
+        #x=np.empty(self.n_features,dtype=np.float32)
 
-        #Tsafir SMA(2)
-        if not self.user_run_time_last.has_key(job.user_id):
-            self.user_run_time_prev[job.user_id] = None
-            self.user_run_time_last[job.user_id] = None
-        if self.user_run_time_prev[job.user_id] != None:
-            average = float((self.user_run_time_last[job.user_id] + self.user_run_time_prev[job.user_id])/ 2)
-            x[0]    = min(job.user_estimated_run_time, average)
-        else:
-            x[0] = job.user_estimated_run_time
+        ##Tsafir SMA(2)
+        #if not self.user_run_time_last.has_key(job.user_id):
+            #self.user_run_time_last1[job.user_id] = None
+            #self.user_run_time_last2[job.user_id] = None
+            #self.user_run_time_last3[job.user_id] = None
+        #if self.user_run_time_prev[job.user_id] != None:
+            #average = float((self.user_run_time_last[job.user_id] + self.user_run_time_prev[job.user_id])/ 2)
+            #x[0]    = min(job.user_estimated_run_time, average)
+        #else:
+            #x[0] = job.user_estimated_run_time
 
-        #Required_time (aka user estimated run time)
-        x[1]= job.user_estimated_run_time
-        return x
-
-    def apply_model(self,x):
-        return np.dot(self.w,x)
+        ##Required_time (aka user estimated run time)
+        #x[1]= job.user_estimated_run_time
+        #return x
 
     def predict(self, job, current_time):
-        x                      = self.make_x(job)
-        job.predicted_run_time = self.apply_model(x)
-
-    def update_model(self,job):
-        x     = self.make_x(job)
-        p     = self.apply_model(x)
-        y     = job.actual_run_time
-        dloss = self.dloss(p,y)
-        self.w     = self.w-self.eta*np.dot(dloss,x)
+        if not self.user_run_time_last.has_key(job.user_id):
+            self.user_run_time_last1[job.user_id] = None
+            self.user_run_time_last2[job.user_id] = None
+            self.user_run_time_last3[job.user_id] = None
 
     def fit(self, job, current_time):
-        self.update_model(job)
-
-        #History for Tsafir SMA(2)
-        assert self.user_run_time_last.has_key(job.user_id) == True
-        assert self.user_run_time_prev.has_key(job.user_id) == True
-        self.user_run_time_prev[job.user_id] = self.user_run_time_last[job.user_id]
-        self.user_run_time_last[job.user_id] = job.actual_run_time
+        #run time history
+        assert self.user_run_time_last1.has_key(job.user_id) == True
+        assert self.user_run_time_last2.has_key(job.user_id) == True
+        assert self.user_run_time_last3.has_key(job.user_id) == True
+        self.user_run_time_last3[job.user_id] = self.user_run_time_last2[job.user_id]
+        self.user_run_time_last2[job.user_id] = self.user_run_time_last1[job.user_id]
+        self.user_run_time_last1[job.user_id] = job.actual_run_time
