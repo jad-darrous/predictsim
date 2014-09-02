80db7b12a378b0ea39eeea91883420f59b441d72
Valentin Reis <valentin.reis@gmail.com>
2014-09-02 11:12:09 +0200

    predictor structure moving

--- simulation/pyss/src/predictors/predictor_sgdlinear.py
+++ simulation/pyss/src/predictors/predictor_sgdlinear.py
@@ -2,43 +2,68 @@ from predictor import Predictor
 import numpy as np
 
 class PredictorSGDLinear(Predictor):
+    #Internal info
     n_features=2
 
     def __init__(self,max_procs=None, max_runtime=None,verbose=True):
-        self.user_run_time_last3 = {}
-        self.user_run_time_last2 = {}
-        self.user_run_time_last1 = {}
+        #Data structures for storing info
+        self.user_job_last3 = {}
+        self.user_job_last2 = {}
+        self.user_job_last1 = {}
+        self.job_x= {}
 
+    def make_x(self,job,running_jobs):
+        """Make a vector from a job. requires job, current time and system state."""
+        x=np.empty(self.n_features,dtype=np.float32)
 
-    #def make_x(self,job):
-        #x=np.empty(self.n_features,dtype=np.float32)
+        #checks on user internal memory
+        if not self.user_run_time_last.has_key(job.user_id):
+            self.user_job_last1[job.user_id] = None
+            self.user_job_last2[job.user_id] = None
+            self.user_job_last3[job.user_id] = None
 
-        ##Tsafir SMA(2)
-        #if not self.user_run_time_last.has_key(job.user_id):
-            #self.user_run_time_last1[job.user_id] = None
-            #self.user_run_time_last2[job.user_id] = None
-            #self.user_run_time_last3[job.user_id] = None
+        #TODO:make x    
         #if self.user_run_time_prev[job.user_id] != None:
             #average = float((self.user_run_time_last[job.user_id] + self.user_run_time_prev[job.user_id])/ 2)
             #x[0]    = min(job.user_estimated_run_time, average)
         #else:
             #x[0] = job.user_estimated_run_time
-
-        ##Required_time (aka user estimated run time)
+        #Required_time (aka user estimated run time)
         #x[1]= job.user_estimated_run_time
-        #return x
+
+        return x
+
+    def store_x(self,job,x):
+        """store x for a given job if its not already stored"""
+        if not x in self.job_x.keys():
+            self.job_x[job]=x
+
+    def pop_x(self, job):
+        """retrieve x for a given job and delete it from memory"""
+        x=self.job_x.pop(x,False)
+        if not x:
+            raise ValueError("Predictor internal x memory failed.")
+        return x
 
     def predict(self, job, current_time):
-        if not self.user_run_time_last.has_key(job.user_id):
-            self.user_run_time_last1[job.user_id] = None
-            self.user_run_time_last2[job.user_id] = None
-            self.user_run_time_last3[job.user_id] = None
+        #make x 
+        x=make_x(job)
+        #store x
+        store_x(job,x)
+        #TODO:make the prediction
 
     def fit(self, job, current_time):
-        #run time history
-        assert self.user_run_time_last1.has_key(job.user_id) == True
-        assert self.user_run_time_last2.has_key(job.user_id) == True
-        assert self.user_run_time_last3.has_key(job.user_id) == True
-        self.user_run_time_last3[job.user_id] = self.user_run_time_last2[job.user_id]
-        self.user_run_time_last2[job.user_id] = self.user_run_time_last1[job.user_id]
-        self.user_run_time_last1[job.user_id] = job.actual_run_time
+        """update the model with this job"""
+        #Pop  x from internal data
+        x=pop_x(job)
+
+        #Updating our data
+        #store user previous run time history
+        assert self.user_job_last1.has_key(job.user_id) == True
+        assert self.user_job_last2.has_key(job.user_id) == True
+        assert self.user_job_last3.has_key(job.user_id) == True
+        self.user_job_last3[job.user_id] = self.user_job_last2[job.user_id]
+        self.user_job_last2[job.user_id] = self.user_job_last1[job.user_id]
+        self.user_job_last1[job.user_id] = job
+
+        #TODO:Fit the model
