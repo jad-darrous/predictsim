24a61efe5b9c25f17a531abb251f808c5f867506
Valentin Reis <valentin.reis@gmail.com>
2014-09-18 09:34:38 +0200

    progressbar

--- simulation/pyss/src/schedulers/simulator.py
+++ simulation/pyss/src/schedulers/simulator.py
@@ -11,6 +11,8 @@ from shrinking_easy_scheduler import ShrinkingEasyScheduler
 import sys
 import os
 
+import progressbar
+
 class Simulator(object):
     """
     Assumption 1: The simulation clock goes only forward. Specifically,
@@ -28,7 +30,7 @@ class Simulator(object):
         self.output_swf = None
 
         self.machine = ValidatingMachine(num_processors=num_processors, event_queue=self.event_queue)
-        
+
         if hasattr(self.scheduler, "I_NEED_A_PREDICTOR") and self.scheduler.I_NEED_A_PREDICTOR:
                 self.scheduler.running_jobs =  self.machine.jobs
 
@@ -47,10 +49,16 @@ class Simulator(object):
 
         if hasattr(scheduler, "I_NEED_A_PREDICTOR") and scheduler.I_NEED_A_PREDICTOR:
             self.event_queue.add_handler(JobPredictionIsOverEvent, self.handle_prediction_event)
-            
+
         for job in self.jobs:
             self.event_queue.add_event( JobSubmissionEvent(job.submit_time, job) )
-        
+
+        widgets = ['Event Queue Size: ', progressbar.Counter(), ' Timer: ',progressbar.Timer(), progressbar.Bar()]
+
+        self.pbar = progressbar.ProgressBar(widgets=widgets, maxval=len(self.event_queue),poll=0.1).start()
+        self.maxqlen=len(self.event_queue)
+
+
 
     def handle_submission_event(self, event):
         assert isinstance(event, JobSubmissionEvent)
@@ -69,10 +77,10 @@ class Simulator(object):
     def store_terminated_job(self, event):
         assert isinstance(event, JobTerminationEvent)
         outl = []
-        
+
         #1. Job Number
         outl.append(str(event.job.id))
-        #2. Submit Time 
+        #2. Submit Time
         outl.append(str(event.job.submit_time))
         #3. Wait Time
         outl.append(str(event.job.start_to_run_at_time - event.job.submit_time))
@@ -106,8 +114,9 @@ class Simulator(object):
         outl.append("-1")
         #18. Think Time
         outl.append("-1")
-        
+
         self.output_swf.write(' '.join(outl)+"\n")
+
 
     def handle_prediction_event(self, event):
         assert isinstance(event, JobPredictionIsOverEvent)
@@ -117,10 +126,12 @@ class Simulator(object):
         if event.job.predicted_run_time < event.job.actual_run_time:
              self.event_queue.add_event(JobPredictionIsOverEvent(job=event.job, timestamp=event.job.predicted_finish_time))
 
-            
+
     def run(self):
         while not self.event_queue.is_empty:
+            self.pbar.update(min(self.maxqlen,len(self.event_queue)))
             self.event_queue.advance()
+
 
 def run_simulator(num_processors, jobs, scheduler, output_swf=None, input_file=None, no_stats=False):
     simulator = Simulator(jobs, num_processors, scheduler, output_swf, input_file)
@@ -134,21 +145,21 @@ def print_simulator_stats(simulator):
     # simulator.scheduler.cpu_snapshot.printCpuSlices()
     print_statistics(simulator.terminated_jobs, simulator.time_of_last_job_submission)
 
-# increasing order 
+# increasing order
 by_finish_time_sort_key   = (
     lambda job : job.finish_time
 )
 
-# decreasing order   
-#sort by: bounded slow down == max(1, (float(wait_time + run_time)/ max(run_time, 10))) 
+# decreasing order
+#sort by: bounded slow down == max(1, (float(wait_time + run_time)/ max(run_time, 10)))
 by_bounded_slow_down_sort_key = (
     lambda job : -max(1, (float(job.start_to_run_at_time - job.submit_time + job.actual_run_time)/max(job.actual_run_time, 10)))
 )
 
-    
+
 def print_statistics(jobs, time_of_last_job_submission):
     assert jobs is not None, "Input file is probably empty."
-    
+
     sum_waits     = 0
     sum_run_times = 0
     sum_slowdowns           = 0.0
@@ -156,29 +167,29 @@ def print_statistics(jobs, time_of_last_job_submission):
     sum_estimated_slowdowns = 0.0
     sum_tail_slowdowns      = 0.0
 
-    
+
     counter = tmp_counter = tail_counter = 0
-    
+
     size = len(jobs)
     precent_of_size = int(size / 100)
-    
+
     for job in sorted(jobs, key=by_finish_time_sort_key):
-        
+
         tmp_counter += 1
 
         if job.user_estimated_run_time == 1 and job.num_required_processors == 1: # ignore tiny jobs for the statistics
             size -= 1
             precent_of_size = int(size / 100)
             continue
-        
+
         if size >= 100 and tmp_counter <= precent_of_size:
             continue
-        
+
         if job.finish_time > time_of_last_job_submission:
-            break 
-        
+            break
+
         counter += 1
-        
+
         wait_time = float(job.start_to_run_at_time - job.submit_time)
         run_time  = float(job.actual_run_time)
         estimated_run_time = float(job.user_estimated_run_time)
@@ -187,46 +198,46 @@ def print_statistics(jobs, time_of_last_job_submission):
         sum_waits += wait_time
         sum_run_times += run_time
         sum_slowdowns += float(wait_time + run_time) / run_time
-        sum_bounded_slowdowns   += max(1, (float(wait_time + run_time)/ max(run_time, 10))) 
+        sum_bounded_slowdowns   += max(1, (float(wait_time + run_time)/ max(run_time, 10)))
         sum_estimated_slowdowns += float(wait_time + run_time) / estimated_run_time
 
         if max(1, (float(wait_time + run_time)/ max(run_time, 10))) >= 3:
             tail_counter += 1
             sum_tail_slowdowns += max(1, (float(wait_time + run_time)/ max(run_time, 10)))
-            
+
     sum_percentile_tail_slowdowns = 0.0
     percentile_counter = counter
-    
+
     for job in sorted(jobs, key=by_bounded_slow_down_sort_key):
         wait_time = float(job.start_to_run_at_time - job.submit_time)
         run_time  = float(job.actual_run_time)
         sum_percentile_tail_slowdowns += float(wait_time + run_time) / run_time
-        percentile_counter -= 1 # decreamenting the counter 
+        percentile_counter -= 1 # decreamenting the counter
         if percentile_counter < (0.9 * counter):
             break
-        
-        
-        
+
+
+
     print
     print "STATISTICS: "
-    
+
     print "Wait (Tw) [minutes]: ", float(sum_waits) / (60 * max(counter, 1))
 
     print "Response time (Tw+Tr) [minutes]: ", float(sum_waits + sum_run_times) / (60 * max(counter, 1))
-    
+
     print "Slowdown (Tw+Tr) / Tr: ", sum_slowdowns / max(counter, 1)
 
     print "Bounded slowdown max(1, (Tw+Tr) / max(10, Tr): ", sum_bounded_slowdowns / max(counter, 1)
-    
+
     print "Estimated slowdown (Tw+Tr) / Te: ", sum_estimated_slowdowns / max(counter, 1)
 
     print "Tail slowdown (if bounded_sld >= 3): ", sum_tail_slowdowns / max(tail_counter, 1)
     print "   Number of jobs in the tail: ", tail_counter
 
-    print "Tail Percentile (the top 10% sld): ", sum_percentile_tail_slowdowns / max(counter - percentile_counter + 1, 1)    
-    
+    print "Tail Percentile (the top 10% sld): ", sum_percentile_tail_slowdowns / max(counter - percentile_counter + 1, 1)
+
     print "Total Number of jobs: ", size
-    
+
     print "Number of jobs used to calculate statistics: ", counter
     print
 
