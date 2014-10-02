a63477b298f4bf8db1122d35e022370dc6b2ba26
Valentin Reis <valentin.reis@gmail.com>
2014-10-02 13:03:58 +0200

    config file and summary script

---
+++ experiments/data/summary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm summary.out
+
+for f in CEA-curie_cut CTC-SP2 KTH-SP2 SDSC-BLUE SDSC-SP2 ;
+do
+  cd $f
+  make simu_metrics
+  cd ..
+  cat $f/sim_analysis/metrics >> summary.out
+done
