e66b580013c836c04234012b722c3b7dc4108a8a
Valentin Reis <valentin.reis@gmail.com>
2015-02-19 11:29:42 +0100

    experiment dict (WIP)

---
+++ experiments/experiment_dicts.py
@@ -0,0 +1,18 @@
+rightside=['abs','square','exp']
+leftside=['abs','square','exp']
+leftparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
+rightparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
+threshold=['0','10','60','600','6400']
+weight=["1+10*log(r/m)","1+10*log(m/r)","1+10*log(1/(r*m))","1+10*log(m*r)"]
+scheduler = [{
+	#"name":'easy_prediction_backfill_scheduler',
+	#The predictor (if needed) to use.
+	#To list them: for s in predictors/predictor_*.py ; do basename -s .py $s; done
+	'predictor': {"name":"predictor_sgdlinear", "max_cores":"auto","eta":5000, "loss":"composite","rightside":rs,"rightparam":rp,"leftside":ls,"leftparam":lp,"threshold":tr, "weight":"1", "quadratic":False,"cubic": False,"gd": "NAG","regularization":"l2","lambda":4000000000},
+
+	#The corrector (if needed) to use.
+	#Choose between: "+str(schedulers.common_correctors.correctors_list())
+        'corrector': {"name":"recursive_doubling"},
+	}
+        for rs in rightside
+        ]
