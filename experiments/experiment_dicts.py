b580a6f5daa94e1862c172a2f7479775e05ec3ee
Valentin Reis <valentin.reis@gmail.com>
2015-02-19 16:14:22 +0100

    experiment dicts for predictors.

--- experiments/experiment_dicts.py
+++ experiments/experiment_dicts.py
@@ -4,14 +4,22 @@ leftparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
 rightparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
 threshold=['-6400','-600','-60','-10','0','10','60','600','6400']
 weight=["1+10*log(r/m)","1+10*log(m/r)","1+10*log(1/(r*m))","1+10*log(m*r)"]
-scheduler = [{
-	#"name":'easy_prediction_backfill_scheduler',
-	#The predictor (if needed) to use.
-	#To list them: for s in predictors/predictor_*.py ; do basename -s .py $s; done
-	'predictor': {"name":"predictor_sgdlinear", "max_cores":"auto","eta":5000, "loss":"composite","rightside":rs,"rightparam":rp,"leftside":ls,"leftparam":lp,"threshold":tr, "weight":"1", "quadratic":False,"cubic": False,"gd": "NAG","regularization":"l2","lambda":4000000000},
-
-	#The corrector (if needed) to use.
-	#Choose between: "+str(schedulers.common_correctors.correctors_list())
+scheduler_configs = [{
+	'predictor': {"name":"predictor_sgdlinear",
+            "max_cores":"auto",
+            "eta":5000,
+            "loss":"composite",
+            "rightside":rs,
+            "rightparam":rp,
+            "leftside":ls,
+            "leftparam":lp,
+            "threshold":tr,
+            "weight":w,
+            "quadratic":True,
+            "cubic": False,
+            "gd": "NAG",
+            "regularization":"l2",
+            "lambda":4000000000},
         'corrector': {"name":"recursive_doubling"},
 	}
         for rs in rightside
@@ -19,6 +27,7 @@ scheduler = [{
         for ls in leftside
         for lp in leftside
         for tr in threshold
+        for w in weight
         if (not rs=="exp" and rp>0.01)
         if (not ls=="exp" and lp>0.01)
         ]
