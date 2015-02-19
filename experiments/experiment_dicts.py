3c4d7b599cd3d50f86a703eae5812f3cbf7eb4e6
Valentin Reis <valentin.reis@gmail.com>
2015-02-19 16:05:36 +0100

    experiment dicts

--- experiments/experiment_dicts.py
+++ experiments/experiment_dicts.py
@@ -2,7 +2,7 @@ rightside=['abs','square','exp']
 leftside=['abs','square','exp']
 leftparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
 rightparam=['0.0001','0,001','0.01','0.1','1','10','100','1000']
-threshold=['0','10','60','600','6400']
+threshold=['-6400','-600','-60','-10','0','10','60','600','6400']
 weight=["1+10*log(r/m)","1+10*log(m/r)","1+10*log(1/(r*m))","1+10*log(m*r)"]
 scheduler = [{
 	#"name":'easy_prediction_backfill_scheduler',
@@ -15,4 +15,10 @@ scheduler = [{
         'corrector': {"name":"recursive_doubling"},
 	}
         for rs in rightside
+        for rp in rightparam
+        for ls in leftside
+        for lp in leftside
+        for tr in threshold
+        if (not rs=="exp" and rp>0.01)
+        if (not ls=="exp" and lp>0.01)
         ]
