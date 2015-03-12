dec3fe8437fecadd91acd64e78a3686798328913
David Glesser <david.glesser@imag.fr>
2015-03-12 12:55:54 +0100

    Add server/client to run expe on multiple hosts.

---
+++ simulation/pyss/src/run_valexpe_client.py
@@ -0,0 +1,184 @@
+
+import time
+import multiprocessing
+import fabric
+from fabric.api import run
+from fabric.api import env
+from fabric.tasks import execute
+from fabric.context_managers import cd
+import os
+import socket
+
+import json
+
+import sys
+import os.path
+from run_simulator import parse_and_run_simulator
+import pprint
+import random
+
+
+
+server_path = "/home/davandg/runtime_estimation/simulation/pyss/src/run_valexpe_server.py"
+
+num_threads = 2
+
+#env.gateway = 'dglesser@access.grid5000.fr'
+#env.hosts = ['dglesser@frontend.rennes.grid5000.fr']
+env.hosts = ['localhost']
+
+my_name = str(socket.gethostname())
+
+
+fabric.state.output['status'] = True
+fabric.state.output['stdout'] = False
+fabric.state.output['warnings'] = False
+fabric.state.output['running'] = False
+fabric.state.output['user'] = False
+fabric.state.output['stderr'] = False
+fabric.state.output['aborts'] = True
+fabric.state.output['debug'] = False
+
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+env.use_ssh_config = True
+
+
+
+thread_counter = multiprocessing.Value('i', 0)
+
+expe_counter = multiprocessing.Value('i', 0)
+
+
+def get_expe_task():
+	with cd(os.path.dirname(server_path)):
+		res = run('python '+server_path+' get '+my_name)
+	return res
+
+def get_expe():
+	res = execute(get_expe_task)
+	#res = {'localhost': "(u'3', u'WAIT', u'None', u'optionssss')"}
+
+	#dont look, it's dirty
+	for r in res:
+		res = res[r]
+		
+	(a,hash,a,state,a,doer,a,options,a) = res.split("'")
+	options = json.loads(options)
+	print (hash,state,doer,options)
+	return (hash,state,doer,options)
+
+
+
+def expe_done_task(hash):
+	with cd(os.path.dirname(server_path)):
+		res = run('python '+server_path+' done '+hash)
+	return res
+
+def expe_done(hash):
+	res = execute(expe_done_task, hash)
+
+
+def expe_error_task(hash):
+	with cd(os.path.dirname(server_path)):
+		res = run('python '+server_path+' error '+hash)
+	return res
+
+def expe_error(hash):
+	res = execute(expe_done_task, hash)
+	
+
+
+def launchExpe(options, worker_id):
+	with expe_counter.get_lock():
+		expe_counter.value += 1
+		myid = expe_counter.value
+	
+	if not ( os.path.isfile(options["output_swf"]) ):
+		print bcolors.WARNING+"Start expe "+str(myid)+" on w"+str(worker_id)+ bcolors.ENDC+" : "+str(options)
+		error = False
+		tempout = sys.stdout
+		sys.stdout = open(options["output_swf"]+".out", 'w')
+		sys.stderr = sys.stdout
+		try:
+			parse_and_run_simulator(options)
+		except Exception,e:
+			print "Exception: "+str(e)
+			error = str(e)
+		sys.stdout = tempout
+		if not error:
+			print bcolors.OKBLUE+"End   epxe "+str(myid)+ bcolors.ENDC
+			return True
+		else:
+			print bcolors.FAIL+"ERROR on "+str(myid)+": "+str(e)+ bcolors.ENDC
+			return False
+	else:
+		print bcolors.OKGREEN+"Already done"+str(myid)+ bcolors.ENDC+" : "+str(options)
+		return True
+
+
+def worker():
+	global thread_counter
+	with thread_counter.get_lock():
+		thread_counter.value += 1
+		worker_id = thread_counter.value
+		print "Start Worker: ", worker_id
+	try:
+		while True:
+			#get a new expe
+			(hash,state,doer,options) = get_expe()
+			
+			#exec it
+			#print "doing", hash
+			#time.sleep(1)
+			err = launchExpe(options, worker_id)
+			
+			#tell it ended
+			if err:
+				expe_error(hash)
+			else:
+				expe_done(hash)
+	finally:
+		with thread_counter.get_lock():
+			thread_counter.value -= 1
+
+
+
+
+def useless_task():
+	run("echo OK")
+
+#we run first a useless task to open the connection
+#execute(useless_task)
+
+
+ts = []
+
+for i in range(num_threads):
+	t = multiprocessing.Process(target=worker)
+	t.daemon = True
+	t.start()
+	ts.append(t)
+
+#this:
+#[x.join() for x in ts]
+#do not allow keyboard interrupts,
+#whereas this allow them:
+while True:
+	time.sleep(1)
+	if thread_counter.value == 0:
+		time.sleep(1)
+		break;
+
+
---
+++ simulation/pyss/src/run_valexpe_server.py
@@ -0,0 +1,315 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+import sqlite3
+import hashlib
+import os
+import time
+
+
+expe_name = "KTH-SP2"
+num_processors = 100
+expe_dir = "../../../experiments/data/"+expe_name
+conn = sqlite3.connect(expe_dir+"/run.db")
+ouput_dir = expe_dir+"/simulations/"
+input_file = expe_dir+"/swf/log.swf"
+
+dict_path = '../../../experiments/experiment_dicts.py'
+dict_path = '../../../experiments/experiment_dicts_kth.py'
+
+
+def nice(s):
+	# transform an dict into something command-line compatible, the ugliest way!
+	return str(s).translate(None, " ':/(){},\"").replace("name", "").replace("predictor_sgdlinear", "gdl").replace("max_coresauto", "").replace("regularizationl2", "").replace("lambda4000000000", "").replace("gdNAG", "")
+
+
+def hash(s):
+	return hashlib.sha1(nice(s)).hexdigest()
+	
+
+def db_init_new_db():
+	
+	exec(open(dict_path).read())
+	configs=[
+		{
+		'input_file': input_file,
+		"num_processors":num_processors,
+		'output_swf': ouput_dir+"res_"+s["name"]+"_"+nice(s["predictor"])+"_"+nice(s["corrector"])+".swf.gz",
+		'stats': False,
+		"scheduler":s
+		}
+		for s in sched_configs]
+	
+	
+	c = conn.cursor()
+
+	# Create table
+	c.execute('''CREATE TABLE expes (hash text, state text, doer text, options text)''')
+
+	# Insert a row of data
+	for conf in configs:
+		c.execute("INSERT INTO expes VALUES (?, 'WAIT', 'None', ?)", (hash(conf), json.dumps(conf)))
+
+	# Save (commit) the changes
+	conn.commit()
+	
+
+def db_init_new_db_dir():
+	
+	exec(open(dict_path).read())
+	nconf = len(sched_configs)
+	nconf_skipped = 0
+	nconf_finished = 0
+	configs=[]
+	for s in sched_configs:
+		output_swf = ouput_dir+"res_"+s["name"]+"_"+nice(s["predictor"])+"_"+nice(s["corrector"])+".swf.gz"
+		if os.path.isfile(output_swf) :
+			wrong = True
+			for line in open(output_swf+".out"):
+				if "Traceback" in line:
+					break
+				if "Exception" in line:
+					break
+				if "Elapsed Time" in line:
+					wrong = False
+					break
+			if wrong:
+				print "rm", output_swf
+				print "rm", output_swf+".out"
+				nconf_skipped += 1
+			else:
+				nconf_finished += 1
+				continue
+		configs.append({
+			'input_file': input_file,
+			"num_processors":num_processors,
+			'output_swf': output_swf,
+			'stats': False,
+			"scheduler":s
+			})
+	print nconf, "expes =", len(configs), "todo +", nconf_skipped, "in error +", nconf_finished, "finished"
+
+	c = conn.cursor()
+
+	# Create table
+	c.execute('''CREATE TABLE expes (hash text, state text, doer text, options text)''')
+
+	# Insert a row of data
+	for conf in configs:
+		c.execute("INSERT INTO expes VALUES (?, 'WAIT', 'None', ?)", (hash(conf), json.dumps(conf)))
+
+	# Save (commit) the changes
+	conn.commit()
+
+
+def db_init_new_db_dumb():
+	c = conn.cursor()
+
+	# Create table
+	c.execute('''CREATE TABLE expes (hash text, state text, doer text, options text)''')
+
+	# Insert a row of data
+	c.execute("INSERT INTO expes VALUES ('1','WAIT', 'None', 'optionssss')")
+	c.execute("INSERT INTO expes VALUES ('2','WAIT', 'None', 'optionssss')")
+	c.execute("INSERT INTO expes VALUES ('3','WAIT', 'None', 'optionssss')")
+
+	# Save (commit) the changes
+	conn.commit()
+
+
+def action_get(doer):
+	c = conn.cursor()
+	c.execute('SELECT * FROM expes WHERE state = "WAIT" ORDER BY hash LIMIT 1')
+	expe = c.fetchone()
+	print(expe)
+	
+	c.execute('UPDATE expes SET state="DOING", doer=? WHERE hash=?', (doer,expe[0]))
+	
+	# Save (commit) the changes
+	conn.commit()
+	
+
+
+def update_hash_doing(expe_h, status):
+	c = conn.cursor()
+	c.execute('SELECT * FROM expes WHERE hash=?', (expe_h,))
+	expe = c.fetchone()
+	print(expe)
+	
+	if expe[1] != u'DOING':
+		print("ERROR: This expe is not DOING (s: %s)" % expe[1])
+		return
+	
+	c.execute('UPDATE expes SET state=? WHERE hash=?', (status,expe_h))
+	
+	
+	# Save (commit) the changes
+	conn.commit()
+
+
+
+def action_done(expe_h):
+	update_hash_doing(expe_h, u'DONE')
+
+
+def action_error(expe_h):
+	update_hash_doing(expe_h, u'ERROR')
+
+
+def action_print(id=None):
+	c = conn.cursor()
+	count = 0
+	sql = 'SELECT * FROM expes'
+	if id != None:
+		sql += ' WHERE hash="'+ str(id)+'"'
+	sql += ' ORDER BY hash'
+	for row in c.execute(sql):
+		print row
+		count += 1
+	print "Number of entries:", count
+
+
+
+def floatToTime(t):
+	return time.strftime("%H:%M:%S", time.gmtime(t))
+
+def action_stats():
+	import numpy as np
+	import re
+	printf = sys.stdout.write
+	
+	exec(open(dict_path).read())
+	nconf = len(sched_configs)
+	nconf_skipped = 0
+	nconf_finished = 0
+	summ = 0
+	mini = 99999999
+	maxi = 0
+	data = []
+	configs=[]
+	for s in sched_configs:
+		output_swf = ouput_dir+"res_"+s["name"]+"_"+nice(s["predictor"])+"_"+nice(s["corrector"])+".swf.gz"
+		if os.path.isfile(output_swf) :
+			wrong = True
+			for line in open(output_swf+".out"):
+				if "Traceback" in line:
+					break
+				if "Exception" in line:
+					break
+				if "Elapsed Time" in line:
+					a = re.findall('([0-9]+):([0-9]+):([0-9]+.[0-9]+)', line)
+					#print a
+					sec = float(a[0][2])
+					minu = float(a[0][1])
+					hour = float(a[0][0])
+					t = sec + minu*60 + hour*3600
+					#print line, t
+					summ += t
+					mini = min(t, mini)
+					maxi = max(t, maxi)
+					data += [t]
+					wrong = False
+					break
+			if wrong:
+				nconf_skipped += 1
+			else:
+				nconf_finished += 1
+				continue
+		configs.append({
+			'input_file': input_file,
+			"num_processors":num_processors,
+			'output_swf': output_swf,
+			'stats': False,
+			"scheduler":s
+			})
+		
+
+	n = nconf_finished
+	if n != 0:
+		print "min: ",floatToTime(mini), "  ave: ",floatToTime(summ/n), "  max: ",floatToTime(maxi), "  n:", n
+
+		histo = np.histogram(data, bins=[0,10*60,40*60,1.5*3600,3*3600,6*3600,12*3600,25*3600,999*3600])
+		histo_range = max(histo[0])
+		n_ticks = 50
+		for i in range(len(histo[0])):
+			printf( "[" + floatToTime(histo[1][i]) + ", "+ floatToTime(histo[1][i+1])+ ") ")
+			printf(str(histo[0][i])+"\t")
+			printf("|")
+			for tick in range(int( histo[0][i] * n_ticks / histo_range)):
+				printf("∎")
+			printf('\n')
+	print nconf, "expes =", len(configs), "todo +", nconf_skipped, "in error +", nconf_finished, "finished"
+
+
+
+def usage():
+	print("""
+		run_valexpe_server.py get "houle"
+			    <doer>
+		
+		run_valexpe_server.py done "hash"
+		
+		run_valexpe_server.py error "hash"
+		
+		run_valexpe_server.py print
+		
+		run_valexpe_server.py check_and_copy
+			cat *.swf | wc -l == ...
+		
+		run_valexpe_server.py init_db
+		
+		run_valexpe_server.py init_db_dir
+		
+		run_valexpe_server.py init_db_dumb
+		
+		run_valexpe_server.py stats
+	""")
+	exit(0)
+
+if len(sys.argv) < 2:
+	usage()
+
+action = sys.argv[1]
+
+if action == "get":
+	if len(sys.argv) != 3:
+		usage()
+	action_get(sys.argv[2])
+elif action == "done":
+	if len(sys.argv) != 3:
+		usage()
+	action_done(sys.argv[2])
+elif action == "error":
+	if len(sys.argv) != 3:
+		usage()
+	action_error(sys.argv[2])
+elif action == "print":
+	if len(sys.argv) == 2:
+		action_print()
+	elif len(sys.argv) == 3:
+		action_print(sys.argv[2])
+	else:
+		usage()
+elif action == "check_and_copy":
+	print("TODO")
+elif action == "init_db":
+	db_init_new_db()
+elif action == "init_db_dir":
+	db_init_new_db_dir()
+elif action == "init_db_dumb":
+	db_init_new_db_dumb()
+elif action == "stats":
+	action_stats()
+else:
+	print("not an  action")
+	usage()
+
+
+
+
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
