964cb65223f98971c488b2555933b89e63809621
Jad <jad.darrous@gmail.com>
2015-04-09 17:08:58 +0200

    Allow to pass weight param to a scheduler
    - The weight param is used by the Maui scheduler, but not with other.
    - Print the output just in case the <verbose> option is True.

--- simulation/pyss/src/run_simulator.py
+++ simulation/pyss/src/run_simulator.py
@@ -4,26 +4,27 @@
 Run the PySS Simulator. You should specify a config file, an enforce input and output swfs over that via commandline if you so desire.
 
 Usage:
-    run_simulator.py <swf_file> <config_file> <output_file> [-i] [-v]
+	run_simulator.py <swf_file> <config_file> <output_file> [-i] [-v]
 
 Options:
-    -h --help                                      Show this help message and exit.
-    -v --verbose                                   Be verbose.
-    -i --interactive                               Interactive mode at key points in script.
+	-h --help                                      Show this help message and exit.
+	-v --verbose                                   Be verbose.
+	-i --interactive                               Interactive mode at key points in script.
 '''
 
 from base.docopt import docopt
 import sys
 
 if __debug__:
-    import warnings
-    #warnings.warn("Running in debug mode, this will be slow... try 'python2.4 -O %s'" % sys.argv[0])
+	import warnings
+	#warnings.warn("Running in debug mode, this will be slow... try 'python2.4 -O %s'" % sys.argv[0])
 
 from base.workload_parser import parse_lines
 from base.prototype import _job_inputs_to_jobs
 from schedulers.simulator import run_simulator
 from schedulers.common import module_to_class
 import optparse
+from optparse import OptionParser
 import schedulers.common_correctors
 
 from datetime import datetime
@@ -69,7 +70,6 @@ def parse_and_run_simulator(options):
 
 
 
-
 	my_module = options["scheduler"]["name"]
 	my_class = module_to_class(my_module)
 
@@ -84,13 +84,13 @@ def parse_and_run_simulator(options):
 	#load the class
 	scheduler_non_instancied = package.__dict__[my_module].__dict__[my_class]
 
-	scheduler = scheduler_non_instancied(options)
-
+	scheduler = scheduler_non_instancied(options, weights_list=options["weights"])
 
 	#if hasattr(scheduler_non_instancied, 'I_NEED_A_PREDICTOR'):
 
 	try:
-		print "...."
+		#print "...."
+
 		starttime = datetime.today()
 		run_simulator(
 			num_processors = options["num_processors"],
@@ -101,26 +101,32 @@ def parse_and_run_simulator(options):
 			no_stats = not(options["stats"]),
 			options = options
 			)
-		print "\n"
-		print "Num of Processors: ", options["num_processors"]
-		print "Input file: ", options["input_file"]
-		print "Scheduler:", type(scheduler)
+		end_time = datetime.today() - starttime
+
+		print "\b" * 50
 		
-		print "Elapsed Time:", datetime.today() - starttime
+		if not options["verbose"] is None and options["verbose"]:
+			print "\n"
+			print "Num of Processors: ", options["num_processors"]
+			print "Input file: ", options["input_file"]
+			print "Scheduler:", type(scheduler)
+			print "Elapsed Time:", end_time
 
 	finally:
 		if input_file is not sys.stdin:
 			input_file.close()
 
+	return end_time
+
 
 if __name__ == "__main__":
-        #Retrieve arguments
-        arguments = docopt(__doc__, version='1.0.0rc2')
+	#Retrieve arguments
+	arguments = docopt(__doc__, version='1.0.0rc2')
 
 	config = {}
 	execfile(arguments["<config_file>"], config)
-        config["input_file"]=arguments["<swf_file>"]
-        config["output_swf"]=arguments["<output_file>"]
+	config["input_file"]=arguments["<swf_file>"]
+	config["output_swf"]=arguments["<output_file>"]
 	# python 3: exec(open("example.conf").read(), config)
-
+	
 	parse_and_run_simulator(config)
