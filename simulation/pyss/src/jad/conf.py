d4f8a75ed27a6d57f1e9bd2c4df210340396c073
Jad <jad.darrous@gmail.com>
2015-05-21 16:46:29 +0200

    customize the training_percentage and training_parts for each log

--- simulation/pyss/src/jad/conf.py
+++ simulation/pyss/src/jad/conf.py
@@ -3,19 +3,24 @@ weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0), (0, 0, 1, 0, 0, 0), (
 #weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0), (0, 0, 1, 0, 0, 0), (0, 0, 0, 0, 0, +1), (0, 0, 0, 0, 0, -1)]
 # weights_options = [(1, 0, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0, 0), (0, 0, 1, 0, 0, 0, 0), (0, 0, 0, 0, 0, +1, 0), (0, 0, 0, 0, 0, -1, 0)]
 
+weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0), (0, 0, 0, 0, 0, +1)]
 
-
-training_percentage = 0.6;
-training_parts = 4;
 
 indices = (2,3,4,5,11)
 indices = (2,3,4,5,9)
 
 
-
 #indices = (2,3,4,5,11, 8,9,15)
 
 
-
-#weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0)]
-#indices = (2,3,4)
+mp = {
+	"CEA-curie_sample_log": (0.6, 4),
+	"KTH-SP2_log": (0.6, 6),
+	"CTC-SP2_log": (0.6, 8),
+	"SDSC-SP2_log": (0.6, 16),
+	"CEA-curie_cut_log": (0.8, 16),
+	"CEA-curie_log": (0.8, 16),
+	"SDSC-BLUE_log": (0.8, 32),
+}
+training_percentage, training_parts = mp[fname.split('.')[0]]
+del mp
