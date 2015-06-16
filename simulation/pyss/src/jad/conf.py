874619cb1538fc965429a09a55c03383ec7d7c5b
Jad <jad.darrous@gmail.com>
2015-06-16 13:37:32 +0200

    new configuration

--- simulation/pyss/src/jad/conf.py
+++ simulation/pyss/src/jad/conf.py
@@ -4,6 +4,7 @@ weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0), (0, 0, 1, 0, 0, 0), (
 # weights_options = [(1, 0, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0, 0), (0, 0, 1, 0, 0, 0, 0), (0, 0, 0, 0, 0, +1, 0), (0, 0, 0, 0, 0, -1, 0)]
 
 weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0), (0, 0, 0, 0, 0, +1)]
+weights_options = [(1, 0, 0, 0, 0, 0), (0, 1, 0, 0, 0, 0)]
 
 
 indices = (2,3,4,5,11)
@@ -14,13 +15,17 @@ indices = (2,3,4,5,9)
 
 
 mp = {
-	"CEA-curie_sample_log": (0.6, 4),
-	"KTH-SP2_log": (0.6, 6),
-	"CTC-SP2_log": (0.6, 8),
-	"SDSC-SP2_log": (0.6, 16),
-	"CEA-curie_cut_log": (0.8, 16),
-	"CEA-curie_log": (0.8, 16),
-	"SDSC-BLUE_log": (0.8, 32),
+	"CEA-curie_sample_log":  4,
+	"CEA-curie_cut_log":  8*8,
+
+	"KTH-SP2_log": 256,
+	"CTC-SP2_log": 256,
+	"SDSC-SP2_log": 512,
+	"SDSC-BLUE_log": 1024,
+	"CEA-curie_log": 1024,
+	"Metacentrum2013": 1024,
 }
-training_percentage, training_parts = mp[fname.split('.')[0]]
+training_parts = mp[fname.split('.')[0]]
 del mp
+
+training_percentage = 0.7
